e99947d67a287891bc62c1f768228dbe7d2251ee
Jade Lin <58515651+j5j1j2j3@users.noreply.github.com>
2021-03-25 10:46:58 +0100

    testing out different ways to access the response.json()

--- information.py
+++ information.py
@@ -1,4 +1,10 @@
 import requests, json
 
-response = requests.get(url='http://localhost:1026/ngsi-ld/v1/entities?type=healthyoperator')
-print(response.json())
+response = requests.get(url='http://localhost:1026/ngsi-ld/v1/entities?id=urn:entities:E5')
+#response_json = response.json()
+json_data = json.loads(response.text)
+for r in json_data:
+       print (r["ergonomics"]["risk"]["value"])
+
+
+print(json_data)
