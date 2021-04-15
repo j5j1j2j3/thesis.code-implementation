e4f45590978fcbf87025a2ba1bbd29807d49077a
Jade Lin <58515651+j5j1j2j3@users.noreply.github.com>
2021-04-15 13:23:32 +0200

    added if condition in information

--- information.py
+++ information.py
@@ -7,21 +7,24 @@ response = requests.get(url)
 json_data = json.loads(response.text)
 for r in json_data:
        risk_value = r["https://github.com/j5j1j2j3/thesis.code-implementation/blob/dd50ed4fa73e1d4830a91756e0d02abacee8bffc/risklevel.json"]["rulaRiskLevel"]["value"]
-       r["https://uri.fiware.org/ns/data-models#Alert"]["severity"]["value"] = "ciao"
-       severity = r["https://uri.fiware.org/ns/data-models#Alert"]["severity"]["value"]
+       if risk_value >= 1.0 and risk_value <= 2.0:
+              r["https://uri.fiware.org/ns/data-models#Alert"]["severity"]["value"] = "neglibible risk"
+       if risk_value >= 3.0 and risk_value <= 4.0:
+              r["https://uri.fiware.org/ns/data-models#Alert"]["severity"]["value"] = "low risk"
+       if risk_value >= 5.0 and risk_value <= 6.0:
+              r["https://uri.fiware.org/ns/data-models#Alert"]["severity"]["value"] = "medium risk"
+       if risk_value >= 6.0:
+              r["https://uri.fiware.org/ns/data-models#Alert"]["severity"]["value"] = "very high risk"
+
 
 
 response2 = requests.post(url='http://localhost:1026/ngsi-ld/v1/entityOperations/update', headers={ 
        "content-type": "application/ld+json"}, data=json.dumps(json_data))
 
-#response3 = requests.get('http://localhost:1026/ngsi-ld/v1/entities?id=urn:entities:E1')
-#json_data3 = json.loads(response3.text)
 
 print(risk_value)
-print(severity)
 #print(json_data)
 print(response2.status_code)
-#print(json_data3)
 
 response = requests.get(url)
 json_data = json.loads(response.text)
